16c584187a7747e8b83642494d6ac54c8335d8aa
Lhtbay <baseralper.yalniz@yandex.ru>
2021-12-27 01:01:22 +0300

    27.12.21.
    Arms_Start

---
+++ Assets/Scripts/Arms_sc.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arms_sc : MonoBehaviour
+{
+    [SerializeField] private Transform _cupToHandTrasform; 
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "event")
+        {
+            print("gg");
+            collision.gameObject.GetComponent<CupCopy_sc>().TakeHand();
+            collision.gameObject.transform.position = _cupToHandTrasform.position;
+            collision.gameObject.transform.parent = this.transform;
+        }
+    }
+}
---
+++ Assets/Scripts/Cup/CupCopy_sc.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupCopy_sc : MonoBehaviour
+{
+    GameObject _beforeObject;
+    public bool IsSell = false;
+
+    private CupEvent_sc _cupEventScript;
+
+    [Header("Settings")]
+    [SerializeField] float _lerpValue;
+    [SerializeField] float _multiplierValue;
+    [SerializeField] float _speed;
+
+    private Vector3 _futurePosition;
+
+    private float _time = 0;
+    private int _listIndexNumber;
+    private bool _removeListBool = true;
+
+    private void Start()
+    {
+        _cupEventScript = GameObject.FindGameObjectWithTag("Player").GetComponent<CupEvent_sc>();
+    }
+
+    private void Update()
+    {
+        if (!IsSell)
+        {
+            _listIndexNumber = _cupEventScript.IndexOfValue(this.gameObject);
+            _beforeObject = _cupEventScript._copyCupsList[_listIndexNumber - 1];
+
+            _futurePosition = new Vector3(
+            Mathf.Lerp(transform.position.x, _beforeObject.transform.position.x, _lerpValue * Time.deltaTime * _multiplierValue),
+            transform.position.y,
+            _beforeObject.transform.position.z + 1);
+            transform.position = _futurePosition;
+        }
+        else
+        {
+            transform.Translate(Vector3.left*Time.deltaTime*_speed);
+            _time += Time.deltaTime;
+
+            if (_removeListBool)
+            {
+                _cupEventScript._copyCupsList.RemoveAt(_listIndexNumber);
+                _removeListBool = false;
+            }
+            
+            if (_time >=1f)
+            {               
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    public void TakeHand()
+    {
+        _cupEventScript._copyCupsList.RemoveAt(_listIndexNumber);
+    }
+
+}
